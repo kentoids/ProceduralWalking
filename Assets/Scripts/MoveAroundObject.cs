17310458b33140e8acd6a91ec01584b1457397c4
kentoids <kentomorita@gmail.com>
2018-03-11 19:10:41 +0900

    added stopping feature

--- Assets/Scripts/MoveAroundObject.cs
+++ Assets/Scripts/MoveAroundObject.cs
@@ -7,12 +7,16 @@ public class MoveAroundObject : MonoBehaviour {
 	public bool moveWhenTouched = true;
 	public float speed = 0.5f;
 	float theta, radius;
+	bool touchingTarget = false;
+	bool stopForCollision = false;
+	bool doDrawingStuff = false;
 	// Use this for initialization
 	void Start () {
 		// theta = 0;
 		// Vector2 forTheta = new Vector2(transform.position.z, transform.position.x);
 		// forTheta.Normalize();
 		// Debug.Log("ratio: " + transform.position.z / transform.position.x);
+		if (GetComponent<MeshRenderer>().enabled) doDrawingStuff = true;
 		
 		Vector3 toTarget = target.transform.position - gameObject.transform.position;
 		toTarget.y = 0;
@@ -24,15 +28,29 @@ public class MoveAroundObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// moveWithArcLength(speed);
+		if (touchingTarget && !stopForCollision) {
+			transform.position = new Vector3(Mathf.Cos(theta) * radius, transform.position.y, Mathf.Sin(theta) * radius);
+			theta += speed / radius;
+			if (theta > Mathf.PI * 2) theta = 0;
+		}
+		if (doDrawingStuff) {
+			if (stopForCollision) GetComponent<Renderer>().material.color = Color.yellow;
+			else GetComponent<Renderer>().material.color = Color.white;
+		}
+		stopForCollision = false;
+		touchingTarget = false;
 	}
 
 	void OnTriggerStay(Collider other) {
 		if (!moveWhenTouched) return;
-		if (!other.transform.IsChildOf(transform.parent)) return;
-		// Debug.Log("theta: " + theta);
-		transform.position = new Vector3(Mathf.Cos(theta) * radius, transform.position.y, Mathf.Sin(theta) * radius);
-		theta += speed / radius;
-		if (theta > Mathf.PI * 2) theta = 0;
+		if (!other.transform.IsChildOf(transform.parent)) {
+			stopForCollision = true;
+			return;
+		}
+		touchingTarget = true;
+		// transform.position = new Vector3(Mathf.Cos(theta) * radius, transform.position.y, Mathf.Sin(theta) * radius);
+		// theta += speed / radius;
+		// if (theta > Mathf.PI * 2) theta = 0;
 	}
 
 	void moveWithArcLength(float arcLength) {
--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Procedural Walking Tool for Unity
 
 # Todo
-- hip is not changing its y position from initial height
+- avoid obstacle in front
 
 # Future Todo
 - go over obstacle
     - cast few rays from current destination to next destination
-    - get highest y and go over that with another extra height 
+    - get highest y and go over that with another extra height
+- hip is not changing its y position from initial height
 - body up and down
